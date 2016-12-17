ce051a91e290886881227b565ea7b6b52da14eb2
psanker <patricksanker@gmail.com>
2016-12-17 17:28:49 -0500

    threw together a testy thingy

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,7 @@
 ;(function(win, doc, j, lo) {
 
   /**
-   * Vec2d function
+   * Vec2d object
    *
    */
   function Vec2d(coords) {
@@ -51,6 +51,15 @@
       }
     },
 
+    subtract: function(vec, write = false) {
+      if (write) {
+        this.x -= vec.x;
+        this.y -= vec.y;
+      } else {
+        return this.add(vec.scale(-1));
+      }
+    },
+
     dot: function(vec) {
       return (this.x * vec.x) + (this.y * vec.y);
     },
@@ -64,20 +73,116 @@
     },
 
     distance: function(vec) {
-      return Math.sqrt(Math.pow(vec.x - this.x, 2) + Math.pow(vec.y - this.y, 2));
+      return vec.subtract(this).norm();
     }
   });
 
   /**
-   * Actor function
+   * Stage object
+   *
+   */
+  function Stage(width, height, actors = []) {
+
+    if (!lo.isNumber(width) || !lo.isNumber(height)) {
+      throw new Exception('Width and height of Stage must be numbers');
+    }
+
+    this._width  = width;
+    this._height = height;
+
+    // Idiot check the defaults, too
+    if (lo.isArray(actors)) {
+      this._actors = actors;
+    } else {
+      this._actors = [];
+    }
+  }
+
+  Stage.prototype = Object.create(Object.prototype, {
+
+    /**
+     * Stage::_rasterizePosition(<actor>)
+     *
+     * PRIVATE
+     *
+     * Stage's real origin is top-left (because browsers),
+     * so we need to translate the coords to bottom-left.
+     *
+     * The rasterization takes a float from 0 to 1 and maps it to the browser space.
+     * Ergo, (0,0) in stage space maps to (0, HEIGHT) in browser space and
+     * (1,1) maps to (WIDTH, 0).
+     *
+     * 'actor' may be an array of Actors or just an Actor
+     */
+    _rasterizePosition: function(actor) {
+
+      if (lo.isArray(actor)) {
+
+        for (let a in actor) {
+          this._rasterizePosition(a);
+        }
+      }
+
+      if (actor instanceof Actor) {
+        let p = actor.pos,
+            o = actor.obj;
+
+        // Coordinate transformation
+        let _x = p.x * this._width,
+            _y = this._height - (p.y * this._height);
+
+        // Apply transformation
+        j(o).offset({left: _x, top: _y});
+      }
+    },
+
+    /**
+     * Stage::_vectorize(actor)
+     *
+     * PRIVATE
+     *
+     * Same idea as Stage::_rasterizePosition() but converting browser space to
+     * Stage space for each 'actor'.
+     *
+     * Should only be called after attaching Actor object to the HTML object
+     *
+     * 'actor' may be an array of Actors or just an Actor
+     */
+    _vectorize: function(actor) {
+
+      if (lo.isArray(actor)) {
+
+        for (let a in actor) {
+          this._vectorize(a);
+        }
+      }
+
+      if (actor instanceof Actor) {
+        let o  = actor.obj;
+
+        let _x = j(o).offset().left,
+            _y = j(o).offset().top;
+
+        actor.pos = new Vec2d({
+          x: _x / this._width,
+          y: 1 - (_y / this._height)
+        });
+      }
+    }
+  });
+
+  /**
+   * Actor object
    *
    */
   function Actor(obj, params) {
     if (lo.isNil(obj)) {
-      throw new Exception('Nil object passed as particle source');
+      throw new Exception('Nil object passed as Actor source');
     }
 
     this.obj = obj;
+
+    // TODO: Rewrite this so that the stage does the coord transform, not the Actor
     this.pos = new Vec2d({
       x: j(obj).offset().left,
       y: j(obj).offset().top
@@ -85,8 +190,9 @@
 
     if (lo.isNil(params)) {
       // Initialize the essential physical properties
-      this.vel  = new Vec2d();
-      this.mass = 1;
+      this.vel     = Actor.defaults.vel;
+      this.mass    = Actor.defaults.mass;
+      this.bounded = Actor.defaults.bounded;
     } else {
       if ('vel' in params) {
         let v = params.vel;
@@ -101,17 +207,29 @@
 
         this.vel = new Vec2d(v);
       } else {
-        this.vel = new Vec2d();
+        this.vel = Actor.defaults.vel;
+      }
+
+      if ('mass' in params && lo.isNumber(params.mass)) {
+        this.mass = params.mass;
+      } else {
+        this.mass = Actor.defaults.mass;
       }
     }
   }
+
+  Actor.defaults = {
+    vel: new Vec2d(),
+    mass: 1,
+    bounded: false
+  };
 
   Actor.prototype = Object.create(Object.prototype, {
 
     /**
      * Actor::move(vec, [additive])
      * Moves the actor to the input Vec2d 'vec'
-     * - If 'additive', 'vec' will add to
+     * - If 'additive', 'vec' will add to the actor's position
      *
      */
     move: function(vec, additive = true) {
@@ -124,7 +242,7 @@
   });
 
   /**
-   * Particle function
+   * Particle object
    *
    */
   function Particle(obj, params) {
@@ -151,4 +269,4 @@
     }
   });
 
-}(window, document, $, _));
+})(window, document, $, _);
